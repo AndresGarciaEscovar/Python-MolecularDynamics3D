615a972a1bd8a0613a24d8df77eb132464e8576e
Andres <andrumen1@yahoo.com>
2023-04-14 15:54:53 -0500

    New functions have been added.

--- code/utilities/utilities_molecule.py
+++ code/utilities/utilities_molecule.py
@@ -9,20 +9,38 @@
 
 # General.
 import numpy
+import numpy as np
 
 # User defined.
-import code.molecular_dynamics.main.atom as atom
+import code.main.atom
 
 # ##############################################################################
 # Functions
 # ##############################################################################
+
+
+# ------------------------------------------------------------------------------
+# Load Functions
+# ------------------------------------------------------------------------------
+
+
+def load_from_file(path: str) -> list:
+    """
+        Loads the molecule atoms from a file.
+
+        :param path: The path from where the molecule will be loaded.
+
+        :return: Returns a list with the atoms loaded.
+    """
+
+
 
 # ------------------------------------------------------------------------------
 # Get Functions
 # ------------------------------------------------------------------------------
 
 
-def get_center_of_geometry(atoms: list[atom.Atom]) -> numpy.ndarray:
+def get_center_of_geometry(atoms: list) -> numpy.ndarray:
     """
         From the given set of coordinates and the radius array, gets the center
         of geometry of the molecule.
@@ -35,7 +53,7 @@ def get_center_of_geometry(atoms: list[atom.Atom]) -> numpy.ndarray:
     return numpy.array([0.0], dtype=float)
 
 
-def get_center_of_mass(atoms: list[atom.Atom]) -> numpy.ndarray:
+def get_center_of_mass(atoms: list) -> numpy.ndarray:
     """
         From the given set of coordinates and the radius array, gets the center
         of mass of the molecule.
@@ -46,6 +64,26 @@ def get_center_of_mass(atoms: list[atom.Atom]) -> numpy.ndarray:
          system.
     """
     return numpy.array([0.0], dtype=float)
+
+
+def get_bounding_radius(atoms: list, shift: np.ndarray = None) -> float:
+    """
+        From the given set of atoms, in the given coordinate system,
+        gets the minimum radius of the sphere that encloses the atom.
+
+        :param atoms: The list with the atoms in the molecule.
+
+        :param shift: A numpy array that represents the shift in the molecule,
+         if any.
+
+        :return: The minimum radius of the sphere that encloses the atom.
+    """
+
+
+
+
+
+
 
 
 # ------------------------------------------------------------------------------
