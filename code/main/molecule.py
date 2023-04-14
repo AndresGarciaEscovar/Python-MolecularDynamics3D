6adfb0f1f9bc9b29a7ad1e7a64af62ca8c9d2856
Andres <andrumen1@yahoo.com>
2023-04-14 17:14:19 -0500

    Added load and save functions.

--- code/main/molecule.py
+++ code/main/molecule.py
@@ -8,13 +8,15 @@
 
 # General.
 import os
-import numpy
+import numpy as np
+import yaml
 
 from pathlib import Path
 from typing import Any
 
 # User defined.
 import code.main.atom as atom
+import code.utilities.utilities_molecule as umolecule
 
 # import molecular_dynamics.main.diffusion_tensor as dt
 # import molecular_dynamics.utilities.utilities_strings as us
@@ -30,10 +32,6 @@ class Molecule:
         Class that represents a rigid molecule made of spheres. A file with the
         molecule information can be provided to load the molecule.
     """
-
-    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-    # Public Interface
-    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
     # ##########################################################################
     # Properties
@@ -83,14 +81,14 @@ class Molecule:
     # ------------------------------------------------------------------------ #
 
     @property
-    def masses(self) -> numpy.ndarray:
+    def masses(self) -> np.ndarray:
         """
             Returns the masses  of all the atoms in the molecule.
 
             :return: The numpy array of the masses of the atoms in the molecule,
              in the order in which the atoms are stored.
         """
-        return numpy.array([x.mass for x in self.atoms], dtype=float)
+        return np.array([x.mass for x in self.atoms], dtype=float)
 
     # ------------------------------------------------------------------------ #
 
@@ -118,31 +116,192 @@ class Molecule:
                 "A non-empty name must be provided to the molecule."
             )
 
-        self.__name = f"{name}"
+        self.__name = f"{name}".strip()
 
     # ------------------------------------------------------------------------ #
 
     @property
-    def names(self) -> tuple:
+    def anames(self) -> tuple:
         """
             Returns the names of all the atoms in the molecule.
 
             :return: The tuple that contains all of the names of the atoms in
              the molecule, in the order in which the atoms are stored.
         """
-        return tuple(x.name for x in self.atoms)
+        return tuple(x.aname for x in self.atoms)
 
     # ------------------------------------------------------------------------ #
 
     @property
-    def radii(self) -> numpy.ndarray:
+    def radii(self) -> np.ndarray:
         """
             Returns the radius of all the atoms in the molecule.
 
             :return: The numpy array of the radius of the atoms in the molecule,
              in the order in which the atoms are stored.
         """
-        return numpy.array([x.radius for x in self.atoms], dtype=float)
+        return np.array([x.radius for x in self.atoms], dtype=float)
+
+    # ##########################################################################
+    # Methods
+    # ##########################################################################
+
+    # --------------------------------------------------------------------------
+    # Atom Methods
+    # --------------------------------------------------------------------------
+
+    def atom_add(
+        self, radius: float, mass: float, coordinates: np.ndarray,
+        aname: str = None, atype: str = None
+    ) -> None:
+        """
+            Adds an atom to the atom list.
+
+            :param radius: The floating point number that represents the radius
+             of the atom.
+
+            :param mass: The floating point number that represents the mass of
+             the atom.
+
+            :param coordinates: The numpy array of floating point numbers that
+             that represent the coordinates of the atom.
+
+            :param aname: The name of the atom; this is an optional parameter.
+
+            :param atype: The type of the atom; this is an optional parameter.
+        """
+
+        # Make into strings.
+        aname = "---" if aname is None else f"{aname}"
+        atype = "---" if atype is None else f"{atype}"
+
+        # Check the atom names are unique.
+        if aname.strip() in self.anames:
+            raise ValueError(
+                f"There is already an atom with the name: '{aname}', in this "
+                f"molecule: {self.name}. Please make sure all the atoms have "
+                f"UNIQUE names."
+            )
+
+        # Add the atom.
+        self.atoms.append(atom.Atom(radius, mass, coordinates, atype, aname))
+
+    def atom_remove(self, index: int) -> None:
+        """
+            Removes the atom at the given index.
+
+            :param index: The index of the atom to be removed.
+        """
+
+        # Validate the index.
+        if index < 0 or index >= len(self):
+            raise ValueError(
+                "The index of the given atom is out of range; i.e., must be a "
+                f"number between 0 and {len(self)}. Requested index: {index}"
+            )
+
+        del self.atoms[index]
+
+    # --------------------------------------------------------------------------
+    # Clean Methods
+    # --------------------------------------------------------------------------
+
+    def clean_atoms(self) -> None:
+        """
+            Removes all the atoms and leaves an empty list.
+        """
+        while len(self) > 0:
+            self.atom_remove(0)
+
+    # --------------------------------------------------------------------------
+    # Load an Save Methods
+    # --------------------------------------------------------------------------
+
+    def load(self) -> None:
+        """
+            Loads the given values.
+
+            :param filename: The name of the file where the data is stored.
+        """
+
+        # Remove all the atoms.
+        self.clean_atoms()
+
+        # Load the file.
+        with open(self.filename, "r") as stream:
+            info = yaml.safe_load(stream)
+
+        # Get the molecule name.
+        self.name = info["molecule_name"]
+
+        # Load the atoms.
+        for name, iatom in info["atoms"].items():
+            # Extract the information.
+            atype = f"{iatom['atype']}"
+            crds = np.array(iatom["coordinates"], dtype=float)
+            mass = float(iatom["mass"])
+            radius = float(iatom["radius"])
+
+            # Add the atom to the system.
+            self.atom_add(radius, mass, crds, name, atype)
+
+    def save(self, path: str) -> None:
+        """
+            Saves the configuration of the molecule to a yaml file, with the
+            given name.
+
+            :param path: The path where to save the yaml file; must have a yaml
+             extension for this to work.
+        """
+
+        # Auxiliary variables.
+        indent = "    "
+
+        # Remove leading and trailing spaces.
+        path = path.strip()
+
+        # Check it is a yaml file.
+        if not path.endswith(".yaml"):
+            raise TypeError(
+                f"The given file location: {path}, doesn't correspond to a "
+                f"yaml file. The file must have a '.yaml' extension."
+            )
+
+        # Basic structure.
+        temp = [
+            "# Coordinates are in Angstrom.",
+            "# Mass is in AMU.",
+            "# Radius is in Angstrom.",
+            "# Atom type (atype) is not a mandatory field; set to '---' by "
+            "default.",
+            f"molecule_name: {self.name}",
+            f"atoms:",
+        ]
+
+        # Ge the information of the atoms:
+        for matom in self.atoms:
+            # Get a LIST of the coordinates.
+            coordinates = ','.join([f"{float(x)}" for x in matom.coordinates])
+
+            # Add the variables.
+            temp.append(f"{indent * 1}{matom.aname}:")
+            temp.append(f"{indent * 2}coordinates: [{coordinates}]")
+            temp.append(f"{indent * 2}mass: {matom.mass}")
+            temp.append(f"{indent * 2}radius: {matom.radius}")
+            temp.append(f"{indent * 2}atype: {matom.atype}")
+
+        # Write the file.
+        with open(path, mode="w", newline="\n") as fl:
+            for line in temp:
+                fl.write(line + "\n")
+
+    # --------------------------------------------------------------------------
+    # Rotate Methods
+    # --------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------
+    # Translate Methods
+    # --------------------------------------------------------------------------
 
     # ##########################################################################
     # Constructor
@@ -165,9 +324,8 @@ class Molecule:
         # Create the basic quantities.
         self.atoms = list()
 
-        # Get the center of mass.
-        # self.com = um.get_com(self.atoms)
-        # Get the center of diffusion, center of mass and center of geometry.
+        # Load the molecule.
+        self.load()
 
     # ##########################################################################
     # Dunder Methods
@@ -199,120 +357,6 @@ class Molecule:
 
         return ""
 
-    # ##########################################################################
-    # Methods
-    # ##########################################################################
-
-    # --------------------------------------------------------------------------
-    # Atom Methods
-    # --------------------------------------------------------------------------
-
-    def atom_add(
-        self, radius: float, mass: float, coordinates: numpy.ndarray
-    ) -> None:
-        """
-            Adds an atom to the atom list.
-
-            :param radius: The floating point number that represents the radius
-             of the atom.
-
-            :param mass: The floating point number that represents the mass of
-             the atom.
-
-            :param coordinates: The numpy array of floating point numbers that
-             that represent the coordinates of the atom.
-        """
-        self.atoms.append(
-            atom.Atom(radius=radius, mass=mass, coordinates=coordinates)
-        )
-
-    def atom_remove(self, index: int) -> None:
-        """
-            Removes the atom at the given index.
-
-            :param index: The index of the atom to be removed.
-        """
-
-        # Validate the index.
-        if index < 0 or index >= len(self):
-            raise ValueError(
-                "The index of the given atom is out of range; i.e., must be a "
-                f"number between 0 and {len(self)}. Requested index: {index}"
-            )
-
-        del self.atoms[index]
-
-    # --------------------------------------------------------------------------
-    # Load Methods
-    # --------------------------------------------------------------------------
-
-    def load(self, dimensions: int, filename: str) -> None:
-        """
-            Loads the given values.
-
-            :param filename: The name of the file where the data is stored.
-        """
-
-        # Remove all the atoms.
-        while len(self) > 0:
-            self.atom_remove(0)
-
-        # Load the molecule.
-        with open(filename, mode='r', newline="\n") as molecule:
-            # Load the molecule.
-            self.name = molecule.readline().split(",")[1]
-
-            # Number of atoms.
-            natoms = int(molecule.readline().split(",")[1])
-
-            # Load the atoms.
-            for i in range(natoms):
-                # Read the lines.
-                line = molecule.readline().split(",")
-
-                # Extract the values.
-                coordinates = numpy.array(
-                    [float(x) for x in line[1:dimensions + 1]], dtype=float
-                )
-                mass = float(line[dimensions + 2])
-                radius = float(line[dimensions + 1])
-
-                # Add the atom.
-                self.atom_add(
-                    radius=radius, mass=mass, coordinates=coordinates
-                )
-
-                # Label the atom.
-                self.atoms[i].name = f"Atom {i + 1}"
-
-    # --------------------------------------------------------------------------
-    # Rotate Methods
-    # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # Translate Methods
-    # --------------------------------------------------------------------------
-
-    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-    # Private Interface
-    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-    # ##########################################################################
-    # Methods
-    # ##########################################################################
-
-    # --------------------------------------------------------------------------
-    # Get Methods
-    # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # Translate Methods
-    # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # Validate Methods
-    # --------------------------------------------------------------------------
-
 
 # ##############################################################################
 #
@@ -320,7 +364,16 @@ class Molecule:
 
 if __name__ == "__main__":
     # Get the absolute path.
-    mpath = f"{Path(os.getcwd(), '..', '..', 'data', 'product.txt').resolve()}"
+    mp0 = f"{Path(os.getcwd(), '..', '..', 'data', 'product.yaml').resolve()}"
+    mp1 = f"{Path(os.getcwd(), '..', '..', 'data', 'reactant.yaml').resolve()}"
 
-    # Get the absolute path.
-    molecule = Molecule(mpath)
+    mp2 = f"{Path(os.getcwd(), '..', '..', 'data', 'product_1.yaml').resolve()}"
+    mp3 = f"{Path(os.getcwd(), '..', '..', 'data', 'reactant_1.yaml').resolve()}"
+
+    tcoordinates = np.array([0, 0, 0], dtype=float)
+
+    # Load using the absolute path.
+    molecule0 = Molecule(mp0)
+    molecule0.save(mp2)
+
+    molecule1 = Molecule(mp1)
