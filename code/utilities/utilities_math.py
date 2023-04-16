196a132226027e6f574f2fc21a0f97896e03f738
Andres <andrumen1@yahoo.com>
2023-04-15 19:00:32 -0500

    Updated the math utilities.

---
+++ code/utilities/utilities_math.py
@@ -0,0 +1,92 @@
+"""
+    File that contains special utilities for vector operations.
+"""
+
+# ##############################################################################
+# Imports
+# ##############################################################################
+
+# General.
+import copy
+
+from numpy import array, dot, ndarray
+
+# ##############################################################################
+# Functions
+# ##############################################################################
+
+# ------------------------------------------------------------------------------
+# Intersect Functions
+# ------------------------------------------------------------------------------
+
+
+def intersect_hspheres(
+    coordinate_0: ndarray, radius_0: float, coordinate_1: ndarray,
+    radius_1: float
+) -> bool:
+    """
+        Returns a boolean flag indicating if two hyper-spheres intersect, given
+        their position in space and their hyper-radius, using the Euclidean norm
+        as the distance function.
+
+        :param coordinate_0: The coordinates of the zeroth hyper-sphere.
+
+        :param radius_0: The radius of the zeroth hyper-sphere.
+
+        :param coordinate_1: The coordinates of the first hyper-sphere.
+
+        :param radius_1: The radius of the first hyper-sphere.
+
+        :return: A boolean flag indicating if two hyper-spheres intersect, given
+         their position in space and their hyper-radius; i.e., their separation
+         is greater than the sum of their radii.
+    """
+    # Difference between the two spheres.
+    difference = coordinate_0 - coordinate_1
+    sradius = (radius_0 + radius_1) ** 2
+
+    return dot(difference, difference) < sradius
+
+
+# ------------------------------------------------------------------------------
+# Symmetrize Functions
+# ------------------------------------------------------------------------------
+
+
+def symmetrize(matrix: ndarray, passes: int = 0):
+    """
+        Symmetrizes the given square matrix.
+
+        :param matrix: The matrix to be symmetrized.
+
+        :param passes: The number of passes that are used to symmetrize before
+         getting the output.
+
+        :return: The numpy array with the matrix symmetrized.
+    """
+
+    # Only integer numbers.
+    cpasses = int(passes)
+
+    # Get a copy of the original matrix.
+    cmatrix = copy.deepcopy(matrix)
+    length = len(cmatrix)
+
+    # Don't symmetrize.
+    if cpasses <= 0:
+        return cmatrix
+
+    # Symmetrize the elements, the given number of times.
+    for k in range(cpasses - 1):
+        for i in range(length):
+            for j in range(i + 1, length):
+                cmatrix[i, j] = (cmatrix[i, j] + cmatrix[j, i]) * 0.5
+                cmatrix[j, i] = (cmatrix[i, j] + cmatrix[j, i]) * 0.5
+
+    # Assign the relevant values.
+    for i in range(length):
+        for j in range(i + 1, length):
+            cmatrix[i, j] = (cmatrix[i, j] + cmatrix[j, i]) * 0.5
+            cmatrix[j, i] = cmatrix[i, j]
+
+    return cmatrix
