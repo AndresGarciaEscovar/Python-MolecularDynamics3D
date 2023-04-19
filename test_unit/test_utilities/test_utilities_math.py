3d9c67cd89568b87dc6726896b6a207406c16d57
Andres <andrumen1@yahoo.com>
2023-04-18 21:47:20 -0500

    Continuing to write the test functions.

--- test_unit/test_utilities/test_utilities_math.py
+++ test_unit/test_utilities/test_utilities_math.py
@@ -8,7 +8,7 @@
 
 # General
 import unittest
-
+from numpy import array
 # User defined.
 import code.utilities.utilities_math as umath
 
@@ -25,12 +25,41 @@ ITERATIONS = 100
 
 class TestAtom(unittest.TestCase):
 
-    def test_get_antisymmetric_matrix(self):
+    def test_get_skew_symmetric_matrix(self):
         """
-            Check
+            Tests that the get_skew_symmetric_matrix method is working properly.
         """
-        pass
 
+        # Loop through the different types of arrays.
+        for mtype in (list, tuple):
+            tarray = mtype([1, 2, 3])
+            with self.assertRaises(TypeError):
+                umath.get_skew_symmetric_matrix(tarray)
+
+            tarray = array(tarray, dtype=float)
+            rmatrix = umath.get_skew_symmetric_matrix(tarray)
+
+            compmatrix = array(
+                [
+                    [0.0, -tarray[2], tarray[1]],
+                    [tarray[2], 0.0, -tarray[0]],
+                    [-tarray[1], tarray[0], 0.0]
+                ], dtype=float
+            )
+
+            # Matrices must be equal, element by element.
+            for i, row in enumerate(compmatrix):
+                for j, column in enumerate(row):
+                    self.assertEqual(column, rmatrix[i, j])
+
+            tarray = array([1, 2, 3, 4], dtype=float)
+            with self.assertRaises(ValueError):
+                umath.get_skew_symmetric_matrix(tarray)
+
+    def test_intersect_spheres(self):
+        """
+            Tests that the function intersect_spheres
+        """
 
 # ##############################################################################
 # Main Program
