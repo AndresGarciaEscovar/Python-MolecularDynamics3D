233e85af94689934504a08fcf6479b80f91586f3
Andres <andrumen1@yahoo.com>
2023-05-25 17:55:05 -0500

    Added functions to validate the diffusion tensor.

--- code/validation/validation_molecule.py
+++ code/validation/validation_molecule.py
@@ -12,6 +12,9 @@
 from numpy import dtype, float64, ndarray
 from typing import Any
 
+# User defined.
+import code.validation.validation_parameters as vparameters
+
 # ##############################################################################
 # Functions
 # ##############################################################################
@@ -20,6 +23,43 @@ from typing import Any
 # ------------------------------------------------------------------------------
 # Is Functions
 # ------------------------------------------------------------------------------
+
+def is_diffusion_tensor(dimensions: int, diffusion_tensor: ndarray) -> None:
+    """
+        Checks that the diffusion tensor has valid dimensions, given the number
+        of dimensions.
+
+        :param dimensions: The dimensionality of the space (e.g., 2D -> 2,
+         3D -> 3, etc.).
+
+        :param diffusion_tensor: The numpy array that represents the diffusion
+         tensor.
+
+        :raise ValueError: If the diffusion tensor doesn't have the correct
+         dimensions.
+    """
+    # Check the dimensions is a positive number.
+    if not isinstance(dimensions, int) or dimensions <= 0:
+        value = f"{dimensions}" if isinstance(dimensions, int) else "---"
+        raise ValueError(
+            f"The number of dimensions must be a positive integer number. Type "
+            f"{type(dimensions)}, value: {value}."
+        )
+
+    # Intended dimensions of the diffusion tensor.
+    length = 2 * dimensions if dimensions > 2 else dimensions + 1
+    length = 1 if dimensions == 1 else length
+
+    # Check the shape of the diffusion tensor.
+    try:
+        vparameters.is_shape_matrix(diffusion_tensor, (length, length))
+
+    except ValueError as e:
+        raise ValueError(
+            f"The diffusion tensor doesn't have the correct dimensions; must "
+            f"be {(length, length)} for a {dimensions}D space. Matrix shape "
+            f"validation {e}."
+        )
 
 
 def is_unique_name(name: str, names: tuple) -> None:
