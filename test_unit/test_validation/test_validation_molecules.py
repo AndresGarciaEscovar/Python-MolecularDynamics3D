bbe72c8807ede3ed58c42986748f6ab2d4a15349
Andres <andrumen1@yahoo.com>
2023-05-25 20:27:23 -0500

    Started adding tests for the molecule utilities.

---
+++ test_unit/test_validation/test_validation_molecules.py
@@ -0,0 +1,55 @@
+"""
+    File that contains the unit test for the different validation functions for
+    molecules.
+"""
+
+# ##############################################################################
+# Imports
+# ##############################################################################
+
+# General
+import unittest
+
+from numpy import array, dtype, float64, ndarray, zeros
+from random import uniform
+
+# User defined.
+import code.validation.validation_molecule as vmolecules
+
+# ##############################################################################
+# Classes
+# ##############################################################################
+
+
+class TestValidationMolecules(unittest.TestCase):
+
+    def test_is_diffusion_tensor(self):
+        """
+            Tests that the function is_diffusion_tensor is working as intended.
+        """
+        # Test several dimensions.
+        for i in range(1, 10):
+            # Number of entries.
+            dims = i if i == 1 else i + 1
+            dims = 2 * i if i > 2 else dims
+
+            # A zero tensor is fine.
+            dtensor = zeros(dims**2, dtype=float).reshape((dims, dims))
+
+            # Must not trow an error.
+            vmolecules.is_diffusion_tensor(i, dtensor)
+
+            # Wrond dimensions.
+            with self.assertRaises(ValueError):
+                vmolecules.is_diffusion_tensor(i + 1, dtensor)
+
+# ##############################################################################
+# Main Program
+# ##############################################################################
+
+
+if __name__ == '__main__':
+    """
+        Runs the main program.
+    """
+    unittest.main()
